d1ed495445d98ec7977f0eb61553d81a07f59705
speedevil123 <133794554+speedevil123@users.noreply.github.com>
2024-02-27 19:19:12 +0700

    Delete с++_labs directory to fix ANSI

--- с++_labs/lab_1/Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-int* readArray(std::istream& in, int& num);
-int findnaim(int* arr, int num);
-void writeArray(std::ostream& fout, int kol);
--- с++_labs/lab_2/Header.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-
-std::string* readArray(std::string path, int& num);
-void printArray(std::ostream& out, std::string* arr, int num);
-std::string* getwords(std::string* arr, int& size);
-void writeResult(std::ostream& out, std::string* words, int num);
